b926c45633f2ef79984229b15f1244ea60d58f7b
CodeChrisB <c.buchberger01@gmail.com>
2020-08-13 21:59:07 +0200

    added missing line text to easier identify the diffrence of the files

--- js/jsonComparing.js
+++ js/jsonComparing.js
@@ -5,37 +5,41 @@ return
 let leftJson = document.getElementById('leftBox').value.split("\n");  
 let rightJson = document.getElementById('rightBox').value.split("\n");  
 let result = ''
-let addedLines = []
+let addedLines = 'Lines that are missing :'
 var missingLines =0
 
 
 for(let i = 0; i< leftJson.length; i++){
-    console.log(getTag(leftJson[i]),getTag(rightJson[i-missingLines]))
+    //console.log(getTag(leftJson[i]),getTag(rightJson[i-missingLines]))
     if(getTag(leftJson[i]) == getTag(rightJson[i-missingLines])){
         result += rightJson[i-missingLines]+'\n'
-        console.log('a',rightJson[i])
+        //console.log('a',rightJson[i])
     }else{
         missingLines++
         if(leftJson[i].indexOf('{') <0 && leftJson[i].indexOf('}') <0){
             var pos = leftJson[i].indexOf(':')
             if(pos>0){
                 result += leftJson[i].substring(0,pos+1)+'\n'
-                console.log('c',leftJson[i].substring(0,pos+1)+'\n')
-                addedLines.push(i)
+               // console.log('c',leftJson[i].substring(0,pos+1)+'\n')
+                addedLines += '\n'+(i+1)
             }
         }else{
             result += leftJson[i]+'\n'
-            console.log('c',leftJson[i])
+            addedLines += '\n'+(i+1)
+            //console.log('c',leftJson[i])
         }
         
     }
 }
 
+document.getElementById('missingBox').style ='display: block'
 document.getElementById('resultBox').style ='display: block'
 document.getElementById('resultBox').value = result
+document.getElementById('missingBox').value = addedLines
 }
 
 function getTag(str){
+    console.log(str)
     var pos = str.indexOf(':')
     if(pos>0){
         return str.substring(0,pos+1)
