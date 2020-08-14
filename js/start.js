7a2d39328194e2d95ccfeb5409146239afaec2fd
CodeChrisB <c.buchberger01@gmail.com>
2020-08-15 00:48:07 +0200

    added missing files

---
+++ js/start.js
@@ -0,0 +1,18 @@
+console.log(
+    '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n'+
+    '           Well you found the\n'+
+    '        secret area of my website\n'+
+    '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n'+
+    "             .--.               \n"+
+    "             |__| .-------.\n"+
+    "             |=.| |.-----.|\n"+
+    "             |--| || CCB ||\n"+
+    "             |  | |'-----'|\n"+
+    "             |__|~')_____(\n"+
+    "   But is this all that can be found?\n"+
+    '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n'
+)
+
+/*
+Well this easter egg will extened one day...
+*/
